063496a175c4e21a814d697038336ecb1eda3331
udhayasuresh <udhaya91raj@gmail.com>
2024-03-25 21:37:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Componenets/NavBar';
+import Home from './Componenets/Home';
+import Aboutus from './Componenets/Aboutus';
+import Skills from './Componenets/Skills';
+import Portfolio from './Componenets/Portfolio';
+import Companies from './Componenets/Companies';
+import Contact from './Componenets/Contact';
+import Footer from './Componenets/Footer';
 
 function App() {
   return (
-    <div>
+    <div className='p-[20px]'>{/*padding*/}
+     
       {/*Nav Bar */}
-
+     <NavBar />
       {/* Intro */}
-
+      <Home />
       {/* About me */}
-
+      <Aboutus />
       {/* Skills */}
-
+      <Skills />
       {/* Portfolio */}
-
+      <Portfolio />
       {/* Companies */}
-
+      <Companies />
       {/* Contact Us */}
-      
+      <Contact />
       {/* Footer */}
+      <Footer />
     </div>
   );
 }
--- src/Componenets/NavBar.jsx
+++ src/Componenets/NavBar.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { logo } from '../assets';
+
 
 const NavBar = () => {
+
+    const [toggle, setToggle]= useState(false);
+
   return (
-    <div>NavBar</div>
+ 
+    <div className='flex justify-between'>
+        <img src={logo} className="h-5"/>
+        <div className='hidden md:flex'>
+            <ul className='flex flex-row'>
+                <li className='mr-4 transition-all ease-in-out hover:scale-110 cursor-pointer'>
+                <a href="/#home">Home</a>
+                </li>
+                <li className='mr-4 transition-all ease-in-out hover:scale-110 cursor-pointer'>
+                <a href="/#aboutme">About Me</a>
+                </li>
+                <li className='mr-4 transition-all ease-in-out hover:scale-110 cursor-pointer'>
+                <a href="/#skills">Skills</a>
+                </li>
+                <li className='mr-4 transition-all ease-in-out hover:scale-110 cursor-pointer'>
+                <a href="/#portfolio">Portfolio</a>
+                    </li>
+                <li className='mr-4 transition-all ease-in-out hover:scale-110 cursor-pointer'>
+                <a href="/#companies">Companies</a>
+                    </li>
+                <li className='mr-4 transition-all ease-in-out hover:scale-110 cursor-pointer'>
+                <a href="/#contact">Contact Us</a>
+                    </li>
+            </ul>
+        </div>
+        <div className='md:hidden lg:hidden'>
+            {!toggle ?
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" onClick={()=>setToggle(!toggle)}
+            viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="w-6 h-6 cursor-pointer md:hidden">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+    </svg>:<svg onClick={()=>setToggle(!toggle)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 cursor-pointer md:hidden h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+    
+          }
+          {toggle?
+          <ul className='flex flex-col mt-8 bg-gray-300 p-2'>
+          <li className="menuItem p-3 hover:scale-110 hover:z-50 transition-all duration-300 ease-in-out">
+            <a href="/#home">Home</a>
+          </li>
+          <li className="menuItem  p-3 hover:scale-110">
+            <a href="/#aboutme">About Me</a>
+          </li>
+          <li className="menuItem p-3 hover:scale-110">
+            <a href="/#skills">Skills</a>
+          </li>
+          <li className='menuItem p-3 hover:scale-110'><a href="/#portfolio">Portfolio</a></li>
+          <li className="menuItem p-3 hover:scale-110">
+            <a href="/#companies">Companies</a>
+          </li>
+          <li className="menuItem p-3 hover:scale-110">
+            <a href="/#contact">Contact Us</a>
+          </li>
+
+      </ul>
+       : null}
+        </div>
+    </div>
+    
   )
 }
 
